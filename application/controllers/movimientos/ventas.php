1ce2f63e66dfe6b507dcdfb0bc4cc065170c29d4
IhanaAnahi <anahimantezana543@gmail.com>
2020-12-08 20:16:57 -0400

    modificando en la carpeta movimientos

--- application/controllers/movimientos/ventas.php
+++ application/controllers/movimientos/ventas.php
@@ -46,27 +46,82 @@ class Ventas extends CI_Controller {
 		$idtipocomprobante = $this->input->post("idtipocomprobante");
 		$idcliente = $this->input->post("idcliente");
 		$idusuario = $this->session->userdata("idusuario");
-
-		$idproductos = $this->input->post("idproductos");
-		$precio = $this->input->post("precios");
+		$idproductos = $this->input->post("idproducto");
+		$precio = $this->input->post("precio");
 		$importes = $this->input->post("importes");
-		$cantidades = $this->input->post("cantidades");
-		
-
+		$cantidades = $this->input->post("cantidad");
 		$data = array(
 			'total' => $total,
 			'idtipoComprobante' => $idtipocomprobante,
 			'idcliente' => $idcliente,
-			'idusuario' => $idusuario,
-			
-		
+			'idusuario' => $idusuario
 		);
 
 		if ($this->Ventas_model->save($data)) {
 			$idventa = $this->Ventas_model->lastID();
 			$this->updateComprobante($idtipocomprobante);
 			$this->save_detalle($idventa,$idproductos,$cantidades,$importes);
-			redirect(base_url()."movimientos/ventas");
+			//--------------------------------------------------------------
+			$this->load->library('pdf');
+			$recibo = $this->Ventas_model->recibocliente($idventa);
+			$recibo = $recibo->result();
+
+			
+			$this->pdf = new Pdf();
+			$this->pdf->AddPage();
+			$this->pdf->AliasNbPages();
+			$this->pdf->SetTitle("Recibo");
+			$this->pdf->SetLeftMargin(15);
+			$this->pdf->SetRightMargin(15);
+			$this->pdf->SetFillColor(210,210,210);
+			$this->pdf->SetFont('Arial','B',11);
+			$this->pdf->Cell(30);
+			$this->pdf->Cell(120,10,'Recibo de Pedido',0,0,'C');
+			//ancho,alto,texto a mostrar,borde(LTRB),0 a la derecha-1 sig linea-2 debajo,centrado
+			$this->pdf->Ln(10); // debe ser exacto
+			// Celdas
+			$this->pdf->Cell(25,5,utf8_decode("Cliente"),'TBLR',0,'L',1);
+			//$this->pdf->Cell(50,5,utf8_decode(" "),0,0,'L',0);
+			$this->pdf->Cell(25,5,utf8_decode("CI"),'TBLR',0,'L',1);
+
+			$this->pdf->Ln(5);
+			foreach ($recibo as $row) {
+				$cliente = $row->nombre;
+				$ci = $row->ci;
+				$this->pdf->Cell(25,5,utf8_decode($cliente),'TBLR',0,'L',0);
+				$this->pdf->Cell(25,5,utf8_decode($ci),'TBLR',0,'L',0);
+				$this->pdf->Ln(5);
+			}
+			$this->pdf->Ln(5);
+			$this->pdf->Ln(5);
+			$this->pdf->Cell(35,5,utf8_decode("Producto"),'TBLR',0,'L',1);
+			$this->pdf->Cell(15,5,utf8_decode("Precio"),'TBLR',0,'L',1);
+			$this->pdf->Cell(10,5,utf8_decode("Talla"),'TBLR',0,'L',1);
+			$this->pdf->Cell(20,5,utf8_decode("Cantidad"),'TBLR',0,'L',1);
+			$this->pdf->Cell(20,5,utf8_decode("Importe"),'TBLR',0,'L',1);
+			$this->pdf->Cell(15,5,utf8_decode("Total"),'TBLR',0,'L',1);
+			$this->pdf->Cell(45,5,utf8_decode("Fecha"),'TBLR',0,'L',1);
+			$this->pdf->Ln(5);
+			$this->pdf->Ln(5);
+			foreach ($recibo as $row2) {
+				$producto = $row2->producto;
+				$talla = $row2->talla;
+				$precio = $row2->precio;
+				$cantidad = $row2->cantidad;
+				$importe = $row2->importe;
+				$total = $row2->total;
+				$fecha = $row2->fechaActualizacion;
+				$this->pdf->Cell(35,5,utf8_decode($producto),'TBLR',0,'L',0);
+				$this->pdf->Cell(10,5,utf8_decode($talla),'TBLR',0,'L',0);
+				$this->pdf->Cell(15,5,utf8_decode($precio),'TBLR',0,'L',0);
+				$this->pdf->Cell(20,5,utf8_decode($cantidad),'TBLR',0,'L',0);
+				$this->pdf->Cell(20,5,utf8_decode($importe),'TBLR',0,'L',0);
+				$this->pdf->Cell(15,5,utf8_decode($total),'TBLR',0,'L',0);
+				$this->pdf->Cell(45,5,utf8_decode($fecha),'TBLR',0,'L',0);
+				$this->pdf->Ln(5);
+			}
+			$this->pdf->Output("recibopedido.pdf","I");
+			//redirect(base_url()."movimientos/ventas");
 
 		}else{
 			redirect(base_url()."movimientos/ventas/add");
@@ -86,20 +141,20 @@ class Ventas extends CI_Controller {
 			$data  = array(
 				'idventa' => $idventa,
 				'idproducto' => $productos[$i], 
-				'cantidad' => $cantidadcompra[$i],
-				'importe'=> $importes[$i]
+				'importe'=> $importes[$i],
+				'cantidad' => $cantidadcompra[$i]
 			);
 			$this->Ventas_model->save_detalle($data);
-			$this->updateProducto($productos[$i],$cantidadcompra[$i]);
+			//$this->updateProducto($productos[$i],$cantidadcompra[$i]);
 		}
 	}
 
 	protected function updateProducto($idproducto,$cantidad){
-		$productoActual = $this->productos_model->getProducto($idproducto);
+		$productoActual = $this->productos_model->recuperarproducto($idproducto);
 		$data = array(
-			'stock' => $productoActual->stock - $cantidad, 
+			'stock' => ($productoActual->stock - $cantidad),
 		);
-		$this->productos_model->update($idproducto,$data);
+		$this->productos_model->modificarproducto($idproducto,$data);
 	}
 
 	public function view(){
